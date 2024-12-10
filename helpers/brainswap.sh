1ed07b31aef4249cb5e65054fda11ab6f8a0381f
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2024-12-10 20:04:59 +0100

    Added verbose switch to brainswap (more quiet now).

--- helpers/brainswap.sh
+++ helpers/brainswap.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+VERBOSE=0
+
 IN="$1"
 OUT="$2"
 SOURCE="$3"
+PREFIX="${4:-user.}"
 
 # echo replace $IN by $OUT:
 SWAP=${SOURCE/$IN/$OUT}
 TARGET=$SWAP
 
-echo ""
-echo "Source: $SOURCE"
-echo "Target: $TARGET"
+if [ $VERBOSE -gt 0 ]; then
+    echo ""
+    echo "Source: $SOURCE"
+    echo "Target: $TARGET"
+    echo "Prefix: $PREFIX"
+fi
 
-CMD="exiftool -j \"$SOURCE\" | json2xattr -t \"$TARGET\" -p 'user.exiftool.' -j -"
-echo "$CMD"
+CMD="exiftool -j \"$SOURCE\" | json2xattr -t \"$TARGET\" -p '$PREFIX' -j -"
+
+if [ $VERBOSE -gt 1 ]; then
+    echo "$CMD"
+fi
 
 eval "$CMD"
